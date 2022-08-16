5763dad2678197b2b701602983bf280a67ea8d82
entest <hai@entest.io>
2022-08-16 02:25:09 +0000

    Initial commit

---
+++ test/apigw-integration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigwIntegration from '../lib/apigw-integration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apigw-integration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigwIntegration.ApigwIntegrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
